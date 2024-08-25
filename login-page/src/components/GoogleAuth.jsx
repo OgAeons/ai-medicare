85213114b08921740ae2f2221ca1c6aff43283cf
OgAeons <opaeons@gmail.com>
2024-08-25 19:12:08 +0530

    Removed the redundant code from googleauth component

--- login-page/src/components/GoogleAuth.jsx
+++ login-page/src/components/GoogleAuth.jsx
@@ -1,19 +1,15 @@
 import React, { useEffect } from 'react'
 import { account } from '../lib/appwriteConfig'
-import { useNavigate } from "react-router-dom"
 
 function GoogleAuth() {
-  const navigate = useNavigate()
-  
   async function handleAuth(e) {
     e.preventDefault()
     try {
-        // Start the OAuth2 session
         account.createOAuth2Session("google", "http://localhost:5173/auth-callback", "http://localhost:5173");
     } catch (error) {
         console.error("Authentication error:", error);
     }
-}
+  }
 
   return (
     <div className="Auth">
