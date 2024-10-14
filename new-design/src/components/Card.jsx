6069c2d45119d68369559a8ab07848d030a6fe77
OgAeons <opaeons@gmail.com>
2024-10-14 14:19:35 +0530

    Fixed some styling for cards

--- new-design/src/components/Card.jsx
+++ new-design/src/components/Card.jsx
@@ -4,13 +4,14 @@ import { Link } from 'react-router-dom'
 function Card(props) {
   return (
     <div className="card" style={{backgroundColor: props.bgColor}}>
+      <Link to={props.link}>
       <div className='card-title'>{props.title}</div>
       <div className='card-info'>{props.info}</div>
       <div className='card-bottom'>
-        <Link to={props.link}><div className='card-arrow'><img src="/icons/right-arrow.png" alt="arrow" height={"40rem"} style={{color: "white"}}/></div></Link>
-        
-        <div className='card-img'><img src={props.img} alt="card-img" height={"150rem"}/></div>
+      <div className='card-arrow'><img src="/icons/right-arrow.png" alt="arrow" height={"40rem"} style={{color: "white"}}/></div>
+      <div className='card-img'><img src={props.img} alt="card-img" height={"150rem"}/></div>
       </div>
+      </Link>
     </div>
   )
 }
